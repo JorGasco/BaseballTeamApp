cbd12a0cf980012c25fcc35ae93d26e4e03f538a
Jorge Gasco <97962858+JorGasco@users.noreply.github.com>
2023-08-30 00:02:38 +0100

    Adding Report function in the main app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -38,6 +38,7 @@ fun start(){
             13-> searchPlayerByName()
             14->searchStatsByHits()
 
+            15->report()
 
 
 
@@ -330,6 +331,21 @@ fun searchStatsByHits() {
     } else {
         println(searchResults)
     }
+}
+
+fun report(){
+    println("""
+        |______________________________________________________________________
+        |                               Team Stats 
+        |                               
+        |Average Age:${players.averageAge()} Years old                                     
+        |Average Height : ${players.averageHeight()} Meters               
+        |Average Weight : ${players.averageWeight()}  Kg
+        |                 
+        |${players.averagePlayerStats()}
+        |
+        |______________________________________________________________________    
+        """.trimMargin("|"))
 }
 
 
